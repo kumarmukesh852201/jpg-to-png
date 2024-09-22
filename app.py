f2aaa126308e6f53e00991fdc39dd9a592e3ba58
kumarmukesh852201 <138164794+kumarmukesh852201@users.noreply.github.com>
2024-09-22 12:00:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,5 +43,5 @@ def convert_image(img):
 def request_entity_too_large(error):
     return "The uploaded file size exceeds the 15 MB limit. Please upload a smaller file.", 413
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
